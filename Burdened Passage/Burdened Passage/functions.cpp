242afb777661f6b92133af61676822a5d3ef7e4a
Rivera-Joe <36744600+Rivera-Joe@users.noreply.github.com>
2019-01-07 06:14:07 -0600

    function source
    removed old source file (would not update to newer one.

--- Burdened Passage/Burdened Passage/functions.cpp
+++ Burdened Passage/Burdened Passage/functions.cpp
@@ -7,5 +7,30 @@ using namespace std;
 void mainMenu() {
 	string name;
 	cin >> name;
+	int MenuChoice;
 
+
+	cout << "\t\t\t\tBurdened Passage - C++ Project \n\n\n" << endl;
+	cout << "1- New Game\n";
+	cout << "2- Load Game\n";
+	cout << "3- Exit Game\n";
+	cout << "What would you like to do? "; cin >> MenuChoice;
+
+	switch (MenuChoice) {
+	case 1:
+		startGame();
+		break;
+	case 2:
+		//send player to new function
+		break;
+	case 3:
+		break;
+
+	}
+}
+void startGame() {
+	cout << "Name: Arthur Morgan";
+	cout << "Race: Human";
+	cout << "Class: Warrior";
+	//Extra stuff
 }
